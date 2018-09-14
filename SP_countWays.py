c096a4401d806b72cda18d4e5a964b2494b37a37
finite <finite.impulse@gmail.com>
2018-09-14 09:34:26 +0200

    Add counWays

--- SP_countWays.py
+++ SP_countWays.py
@@ -19,3 +19,13 @@ def countWays(n, memo = {}):
         return memo[n]
     memo[n] = countWays(n-1, memo) + countWays(n-2, memo) + countWays(n-3, memo)
     return memo[n]
+
+# Memoize Bottum Up - Time complexity O(n)
+def countWays(n) :
+    memo = dict()
+    memo[0] = 1
+    memo[1] = 1
+    memo[2] = 2
+    for i in range(3, n + 1) :
+        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+    return memo[n]
