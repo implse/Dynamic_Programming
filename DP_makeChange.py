779a259816dc7e7a5147c7a21e90d9f607d9a7b5
Finite <finite.impulse@gmail.com>
2018-09-24 07:44:23 +0200

    Add Making Change

---
+++ DP_makeChange.py
@@ -0,0 +1,67 @@
+# Given an integer representing a specific amount of change and a set of coin sizes, determine the minimum number of coins required to make that amount of change. You may assume there is always a 1 cent coin.
+
+# coins = (1, 5, 10, 25)
+# change(1) = 1 (1)
+# change(6) = 2 (5 + 1)
+# change(47) = 5 (25 + 10 + 10 + 1 + 1)
+
+# Brute Force:
+
+# Recursively try every possible combination to find the smallest.
+
+# Each recursive step, we will try each coin possibility that is less than the remaining total.
+
+# Substract the coin value from the total remaining until the remaining coin value is 0
+
+
+import math
+
+# Brute Force Recursive
+def makeChange(amount, coins):
+  if(amount == 0):
+    return 0
+  minCoins = math.inf
+
+  for coin in coins:
+    if (amount - coin >= 0):
+      currMin = makeChange(amount - coin, coins)
+      minCoins = min(currMin, minCoins)
+  return minCoins + 1
+
+# DP Memoize Top-Down
+def makeChange(amount, coins):
+  memo = [0]*(amount + 1)
+
+  def change(amount, coins, memo):
+    if memo[amount] == 0:
+      if(amount == 0):
+        return 0
+      minCoins = math.inf
+      for coin in coins:
+        if (amount - coin >= 0):
+          currMin = makeChange(amount - coin, coins)
+          minCoins = min(currMin, minCoins)
+      memo[amount] = minCoins + 1
+    return memo[amount]
+
+  return change(amount, coins, memo)
+
+# DP Memoize Bottom-Up
+def makeChange(amount, coins):
+  memo = [0]*(amount + 1)
+  for i in range(1, amount + 1):
+      minCoins = math.inf
+      for coin in coins:
+        if i - coin >= 0:
+          minCoins = min(minCoins, memo[i - coin])
+      memo[i] = minCoins + 1
+  return memo[amount]
+
+
+
+# Test
+coins = [1, 5, 10, 25]
+print(makeChange(47, coins))
+
+coins = [10, 6, 1]
+print(makeChange(12, coins))
