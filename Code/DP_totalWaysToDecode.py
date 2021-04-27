dadad64b965c66ce1e0c067693ac7aa8c272d1a2
finite <finite.impulse@gmail.com>
2021-04-27 04:44:46 +0200

    Bottom Up solution

--- Code/DP_totalWaysToDecode.py
+++ Code/DP_totalWaysToDecode.py
@@ -4,35 +4,70 @@
 
 # Determine the total number of ways the message can be decoded
 
-# DP Top Down
-def decode_ways(message):
-    dp = [-1 for i in range(len(message) + 1)]
-    dp[0] = 1
-    dp[1] = 0 if message[0] == "0" else 1
 
-    for i in range(1, len(message)):
-        one_digit = int(message[i])
-        #print(one_digit)
-        two_digit = int(message[i - 1] + message[i])
-        #print(two_digit)
-        if one_digit >= 1:
-            dp[i] += dp[i - 1]
-        if two_digit >= 10 and two_digit <= 26:
-            dp[i] = dp[i - 2]
-    #print(dp)
-    return dp[-1]
+# Dynamic Programming : Time O(n) / Space O(n)
+from collections import defaultdict
+
+def decode_ways(message):
+    cache = defaultdict(int)
+    cache[len(message)] = 1
+
+    for i in reversed(range(len(message))):
+        if message[i].startswith("0"):
+            cache[i] = 0
+        elif i == len(message) - 1:
+            cache[i] = 1
+        else:
+            if int(message[i:i + 2]) <= 26:
+                cache[i] = cache[i + 2]
+            cache[i] += cache[i + 1]
+    return cache[0]
+
+#Test
+message_1 = "2263" # (2, 2, 6, 3) or (22, 6, 3) or (2, 26, 3)
+message_2 = "21" # (21) or (2, 1)
+message_3 = "10" # (10)
+message_4 = "14523" # (1, 4, 5, 2, 3) or (14, 5, 2, 3) or (14, 5, 23), or (1, 4, 5, 23)
+
+print(decode_ways(message_1))
+print(decode_ways(message_2))
+print(decode_ways(message_3))
+print(decode_ways(message_4))
+
+
+# Dynamic Programming Bottom Up : Time O(n) / Space O(n)
+def decode_ways(message):
+    dp = [0 for i in range(len(message) + 1)]
+    dp[0] = 1
+    if message[0] == "0":
+        return 0
+    else:
+        dp[1] = 1
+    for i in range(2, len(message) + 1):
+        if message[i - 1] == "0":
+            if message[i - 2] == "1" or message[i - 2] == "2":
+                dp[i] = dp[i - 2]
+            else:
+                return 0
+        else:
+            if message[i - 2] == "1" or message[i - 2] == "2" and int(message[i - 1]) < 7:
+                dp[i] = dp[i - 2] + dp[i - 1]
+            else:
+                dp[i] += dp[i - 1]
+    return dp[len(message)]
+
 
 
 # Test
-# message_1 = "2263" # (2, 2, 6, 3) or (22, 6, 3) or (2, 26, 3)
-# message_2 = "21" # (21) or (2, 1)
-# message_3 = "10" # (10)
-# message_4 = "14523" # (1, 4, 5, 2, 3) or (14, 5, 2, 3) or (14, 5, 23), or (1, 4, 5, 23)
-#
-# print(decode_ways(message_1))
-# print(decode_ways(message_2))
-# print(decode_ways(message_3))
-# print(decode_ways(message_4))
+message_1 = "2263" # (2, 2, 6, 3) or (22, 6, 3) or (2, 26, 3)
+message_2 = "21" # (21) or (2, 1)
+message_3 = "10" # (10)
+message_4 = "14523" # (1, 4, 5, 2, 3) or (14, 5, 2, 3) or (14, 5, 23), or (1, 4, 5, 23)
+
+print(decode_ways(message_1))
+print(decode_ways(message_2))
+print(decode_ways(message_3))
+print(decode_ways(message_4))
 
 
 # Brute Force : Time Complexity 0(2^n) / Space Complexity O(n)
